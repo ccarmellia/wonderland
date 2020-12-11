540c1795677c470a68d0c3fb63fa8863dd1e9e26
ccarmellia <1349074040@qq.com>
2020-12-11 18:34:18 +0800

    modify homePage

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "antd/dist/antd.css";
+import joinLogo from "../assets/joinLogo.png";
+import squareLogo from "../assets/squareLogo.png";
+import userLogo from "../assets/userLogo.png";
 import { Link } from "react-router-dom";
-import { Menu } from "antd";
+import { Menu, Image } from "antd";
 
 export class Navigation extends React.Component {
   state = {
@@ -16,21 +19,34 @@ export class Navigation extends React.Component {
   render() {
     const { current } = this.state;
     return (
-      <div className="Navigation">
+      <div
+        className="Navigation"
+        style={{
+          display: "flex",
+          width: "100%",
+        }}
+      >
         <Menu
+          style={{ width: "100%",display:"flex",justifyContent: "space-around" }}
           onClick={this.handleClick}
           selectedKeys={[current]}
           mode="horizontal"
         >
           <Menu.Item key="1">
-            <Link to="/home">home</Link>
+            <Link to="/home">
+              <Image src={joinLogo} height={58} width={40} />
+            </Link>
           </Menu.Item>
           <Menu.Item key="2">
-            <Link to="/square">square</Link>
+            <Link to="/square">
+              <Image src={squareLogo} height={58} width={40} />
+            </Link>
           </Menu.Item>
 
           <Menu.Item key="3">
-            <Link to="/user">user</Link>
+            <Link to="/user">
+              <Image src={userLogo} height={58} width={40} />
+            </Link>
           </Menu.Item>
         </Menu>
       </div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,35 +2,45 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./home.css";
-import {Navigation} from "../../components/navigation"
-import { List, Avatar, Input } from "antd";
+import houseLogo from "../../assets/houseLogo.png"
+import { Navigation } from "../../components/navigation";
+import { List, Image, Input } from "antd";
 
 const data = [
   {
-    title: "高等数学",
-    description: "从入门到入土",
+    type: "book",
+    title: "红与黑",
+    description: "《红与黑》是法国作家司汤达创作的长篇小说，也是其代表作。",
   },
   {
+    type: "video",
     title: "红高粱",
     description:
-      "《红高粱》是一部表现高密人民在抗日战争中的顽强生命力和充满血性与民族精神的经典之作。",
+      "《《红高粱》是由西安电影制片厂出品的战争文艺片，由张艺谋执导，姜文、巩俐、滕汝骏等主演，于1987年在中国上映。",
   },
   {
-    title: "红苹果",
+    type: "music",
+    title: "红玫瑰",
     description:
-      "爷爷为孙女讲述一个故事，叫做《当还有苹果的时候》，然后，一个美好的世界展开了。",
+      "《红玫瑰》是香港流行男歌手陈奕迅的一首国语歌曲，由李焯雄填词，梁翘柏编曲，收录于2007年发行的专辑《认了吧》。",
   },
   {
-    title: "红舞鞋",
-    description: "关于一个被强迫在红舞鞋里跳舞的女孩的故事。",
-  },
-  {
+    type: "book",
     title: "红楼梦",
-    description: "《红楼梦》，中国古代章回体长篇小说，中国古典四大名著之一，一般认为是清代作家曹雪芹所著。",
+    description:
+      "《红楼梦》，中国古代章回体长篇小说，中国古典四大名著之一，一般认为是清代作家曹雪芹所著。",
   },
   {
-    title:"红豆",
-    description:"《红豆》是王菲演唱的一首歌曲，由林夕作词、柳重言作曲，收录于王菲1998年发行的专辑《唱游》当中。"
+    type: "video",
+    title: "红辣椒",
+    description:
+      "《红辣椒》改编自筒井康隆的封笔之作《梦侦探》，讲述美女医师千叶敦子与天才科学家时田浩作一起发明了“DC MINI”从而引发的故事。",
+  },
+  {
+    type: "music",
+    title: "红莲华",
+    description:
+      "《红莲华》是由LiSA演唱的一首歌曲，由LiSA作词，草野华余子作曲，江口亮编曲，是TV动画《鬼灭之刃》的片头曲。",
   },
 ];
 
@@ -51,18 +61,26 @@ export const Home = () => {
       </div>
       <div className="listWrapper">
         <List
-        className="listStyle"
+          className="listStyle"
           itemLayout="horizontal"
           dataSource={data}
           renderItem={(item) => (
-            <List.Item className="listItemStyle">
+            <List.Item
+              className={
+                item.type === "book"
+                  ? "listItemStyle-book"
+                  : item.type === "video"
+                  ? "listItemStyle-video"
+                  : "listItemStyle-music"
+              }
+            >
               <List.Item.Meta
                 avatar={
-                  <Avatar
-                    className="listAvatar"
-                    shape="square"
-                    size="large"
-                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                  <Image
+                  className="listImage"
+                    width={130}
+                    height={140}
+                    src={houseLogo}
                   />
                 }
                 title={<a href="https://ant.design">{item.title}</a>}
@@ -72,7 +90,10 @@ export const Home = () => {
           )}
         />
       </div>
-        <Navigation className="navigaiton"/>
+      <div className="blankContainer"></div>
+      <div>
+        <Navigation />
+      </div>
     </div>
   );
 };
